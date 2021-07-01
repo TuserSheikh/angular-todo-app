62f48fddd3df97b540ac584ed50018ed6c33a0f6
Md Tuser Sheikh <tuser.sheikh@zoho.com>
2021-07-02 02:05:40 +0600

    delete list and highlight active list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListComponent } from './list/list.component';
+import { TaskComponent } from './list/task/task.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  { path: 'list', component: ListComponent },
+  {
+    path: 'list',
+    component: ListComponent,
+    children: [
+      {
+        path: ':id',
+        component: TaskComponent,
+      },
+    ],
+  },
+  { path: '', redirectTo: '/list', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent },
 ];
 
--- src/app/list/task/task.component.ts
+++ src/app/list/task/task.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ListService } from '../list.service';
 
 @Component({
   selector: 'app-task',
@@ -6,11 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task.component.scss'],
 })
 export class TaskComponent implements OnInit {
-  constructor() {}
+  listId!: number;
+  constructor(
+    private _route: ActivatedRoute,
+    private _listService: ListService
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this._route.paramMap.subscribe((paramMap) => {
+      const id = paramMap.get('id');
+      if (id) {
+        this.listId = +id;
+      }
+    });
+  }
 
   deleteList() {
-    console.log('list deleted');
+    this._listService.removeList(this.listId);
   }
 }
